bd302caf0525f190d20b03ef973e8d1c38f0d770
Narciso <narciso.benigno@gmail.com>
2026-01-19 14:53:44 +1100

    feat: implement getArnieQuotes

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -1,10 +1,21 @@
-const { httpGet } = require('./mock-http-interface');
+const {httpGet} = require('./mock-http-interface');
 
 const getArnieQuotes = async (urls) => {
-  // TODO: Implement this function.
-  // return results;
+    return Promise.all(
+        urls.map(httpGet)
+            .map(response => response.then(handleResponse))
+    )
 };
 
+const handleResponse = (response) => {
+    const {message} = JSON.parse(response.body)
+    if (response.status === 200) {
+        return {'Arnie Quote': message}
+    } else {
+        return {'FAILURE': message}
+    }
+}
+
 module.exports = {
-  getArnieQuotes,
+    getArnieQuotes,
 };
